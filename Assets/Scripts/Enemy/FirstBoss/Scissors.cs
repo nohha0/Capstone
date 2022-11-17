d1d03dbe3d4f2804f4095dd1f2c299fb020931fc
nohha0 <114228512+nohha0@users.noreply.github.com>
2022-11-17 18:29:41 +0900

    FirstBoss

--- Assets/Scripts/Enemy/FirstBoss/Scissors.cs
+++ Assets/Scripts/Enemy/FirstBoss/Scissors.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class Scissors : MonoBehaviour
 {
@@ -10,21 +11,37 @@ public class Scissors : MonoBehaviour
     Vector2 distance;
     public float throwSpeed;
     public float upForce;
+    public int rotateForce;
 
     float lookTime;
+    float rand;
+
 
     void Start()
     {
         targetGameObject = GameObject.FindWithTag("Player");
         rigid = GetComponent<Rigidbody2D>();
+
+        distance = targetGameObject.transform.position - transform.position;
+        rand = Random.Range(0.9f, 1.1f);
+
+        rigid.AddForce(new Vector2(distance.x * (throwSpeed * rand), upForce));
+        Invoke("DestroyScissors", 1);
     }
 
     void Update()
     {
-        distance = targetGameObject.transform.position - transform.position;
-        rigid.AddForce(new Vector2(distance.x * throwSpeed, upForce));
+        transform.Rotate(new Vector3(0, 0, rotateForce));
+
+        //distance = targetGameObject.transform.position - transform.position;
+        //rigid.AddForce(new Vector2(distance.x * throwSpeed, upForce));
         //lookTime += Time.deltaTime;
         //if (lookTime <= 5f)
         //transform.LookAt(targetGameObject.transform);
+    }
+
+    void DestroyScissors()
+    {
+        Destroy(gameObject);
     }
 }
