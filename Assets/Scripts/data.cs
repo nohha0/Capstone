df08b2ad452e3fccf7e554248b66170efad9d6ac
Shin004 <115400628+Shin004@users.noreply.github.com>
2022-12-01 22:58:46 +0900

    세팅 설정 생각
    아 이거 좀 생각보다 걸리겠는데 아직 세팅이 이상해서 다시 생각해봐야겠음
    내일 문 스테이지
    세팅부터 하고

--- Assets/Scripts/data.cs
+++ Assets/Scripts/data.cs
@@ -7,6 +7,8 @@ public class data : MonoBehaviour
     public int Lcount;             //왼쪽으로 공격 시작
     public int Rcount;             //오른쪽으로 공격시작
 
+    int Stage = 1;
+    //1:화장실 2:주방 3:거실 4:복도 가로, 5: 복도 세로, 6:거실,7:작은방, 8:옷장
     private void Start()
     {
         Lcount = 0;
