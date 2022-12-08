cf68b63d23497901627dbbb9ac1854e23a2c5f72
Shin004 <115400628+Shin004@users.noreply.github.com>
2022-12-09 00:49:12 +0900

    이펙트

---
+++ Assets/Scripts/Stage/EnemyRespwon.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRespwon : MonoBehaviour
+{
+
+    public Vector2[] points;
+
+    public GameObject monsterPrefab;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
